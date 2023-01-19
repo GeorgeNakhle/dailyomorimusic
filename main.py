42cd9439ed3f7c196fdcfb612b0bbc9872c1c1a1
George Nakhle <1229gn@gmail.com>
2023-01-19 01:36:45 -0800

    Working build w/o scheduling

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 import tweepy
 import keys
 import os
+import random
+import shutil
 from tinytag import TinyTag
+import schedule
+import time
 
 client = tweepy.Client(keys.bearer_token, keys.api_key, keys.api_secret, keys.access_token, keys.access_token_secret)
 auth = tweepy.OAuth1UserHandler(keys.api_key, keys.api_secret, keys.access_token, keys.access_token_secret)
 api = tweepy.API(auth)
 
-def get_all_songs(folder):
-    songs = []
-    for file in os.listdir(folder):
-        songs.append(file[16:-4])
-    print(songs)
+OST_FOLDER = "./ost"
+USED_FOLDER = "./ost-used"
 
-def get_all_composers(folder):
-    songs = []
-    for file in os.listdir(folder):
-        song = TinyTag.get(folder + '/' + file)
-        print('Composer: ' + song.artist)
-        songs.append(song.artist)
-    #print(songs)
+#region FILE EXPLORER
 
-def create_text(song_name, composer_name):
-    print('text created')
+def get_random_file(folder):
+    return random.choice(os.listdir(folder))
 
-def post_tweet(file):
-    media = api.media_upload(file)
+def move_file(file, folder):
+    shutil.move(file, folder)
+
+#endregion
+
+#region TWITTER
+
+def get_title(file):
+        return file[16:-4]
+
+def get_composition(file):
+        song = TinyTag.get(OST_FOLDER + "/" + file)
+        return song.artist.replace("/", ", ")
+
+def create_tweet_text(file):
+    title = get_title(file)
+    composition = get_composition(file)
+    tweet_text = title + "\n\nComposition: " + composition
+    return tweet_text
+
+def post_tweet(text, file):
+    media = api.media_upload(filename=file, media_category="tweet_video")
     api.update_status(text, media_ids = [media.media_id_string])
+    print("Tweeted successfully!")
 
-#post_tweet('./ost/OMORI OST - 008 Trouble Brewing.mp4')
-#get_all_songs('./ost')
-get_all_composers('./ost')
-print("Tweeted successfully!")
+#endregion
+
+today_file = get_random_file(OST_FOLDER)
+print(create_tweet_text(today_file))
+post_tweet(create_tweet_text(today_file), OST_FOLDER + '/' + today_file)
+move_file(OST_FOLDER + '/' + today_file, USED_FOLDER)
