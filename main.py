af516904f4b7aa9768771dac249c802f9e89f130
George Nakhle <1229gn@gmail.com>
2023-01-18 21:46:04 -0800

    Changed OST names

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tweepy
 import keys
+import ffmpeg
+import sys
+from pprint import pprint
 
 def api():
     auth = tweepy.OAuth1UserHandler(keys.api_key, keys.api_secret)
@@ -17,7 +20,14 @@ def tweet(api: tweepy.API, message: str, image_path=None):
 
 if __name__ == '__main__':
     api = api()
-    tweet(api, 'This was tweeted from Python', './ost/dailyomorimusic-pfp.jpg')
+
+    # read the audio/video file from the command line arguments
+    media_file = ''./ost/OMORI OST - 001 Title [9d9e6XmNn9Q].mp4'
+    # uses ffprobe command to extract all possible metadata from the media file
+    pprint(ffmpeg.probe(media_file)["streams"])
+
+
+    #tweet(api, 'This was tweeted from Python', './ost/dailyomorimusic-pfp.jpg')
 
 #Todo
 #https://omori.fandom.com/wiki/MUSIC add composer (change file metadataa?)
